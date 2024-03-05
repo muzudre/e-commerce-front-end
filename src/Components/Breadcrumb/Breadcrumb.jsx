30e29436f64c4982a986dca720c4c5bece3fc8ce
Muslim Zabirov <muzudre@gmail.com>
2024-03-05 10:21:20 +0500

    complete breadcrumb component

---
+++ src/Components/Breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Breadcrumb.css";
+import arrow_icon from "../Assets/breadcrum_arrow.png";
+
+const Breadcrumb = (props) => {
+  const { product } = props;
+  return (
+    <div className="breadcrumb">
+      Home <img src={arrow_icon} alt="" /> Shop <img src={arrow_icon} alt="" />{" "}
+      {product.category} <img src={arrow_icon} alt="" /> {product.name}
+    </div>
+  );
+};
+
+export default Breadcrumb;
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrumb from "../Components/Breadcrumb/Breadcrumb";
 
 const Product = () => {
-  return <div>Product</div>;
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+
+  return (
+    <div className="product">
+      <Breadcrumb product={product} />
+    </div>
+  );
 };
 
 export default Product;
