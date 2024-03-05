68fd0e846fdc760ab5af6b5e884aff7035a620ea
Muslim Zabirov <muzudre@gmail.com>
2024-03-05 10:09:11 +0500

    complete login  page

--- src/Pages/LoginSingUp.jsx
+++ src/Pages/LoginSingUp.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "./CSS/LoginSignUp.css";
 
 const LoginSingUp = () => {
-  return <div>LoginSingUp</div>;
+  return (
+    <div className="login-sign-up">
+      <div className="login-sign-up-container">
+        <h1>Sign up</h1>
+        <div className="login-sign-up-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="emil" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="login-sign-up-login">
+          Already have an account? <span>Login</span>
+        </p>
+        <div className="login-sign-up-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, I agree to the terms of use and privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginSingUp;
