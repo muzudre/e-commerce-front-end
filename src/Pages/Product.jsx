15fc3389d1dfaaeb9b8db02aa6b59bc70675293a
Muslim Zabirov <muzudre@gmail.com>
2024-03-12 21:52:41 +0500

    complete product display page with breadcrumb

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { ShopContext } from "../Context/ShopContext";
 import { useParams } from "react-router-dom";
 import Breadcrumb from "../Components/Breadcrumb/Breadcrumb";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 
 const Product = () => {
   const { all_product } = useContext(ShopContext);
@@ -11,6 +12,7 @@ const Product = () => {
   return (
     <div className="product">
       <Breadcrumb product={product} />
+      <ProductDisplay product={product} />
     </div>
   );
 };
